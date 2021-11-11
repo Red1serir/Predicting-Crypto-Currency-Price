04e46b90e4f7a42fda47799bca8a5f0f9c0784f4
Red1serir <meritatan964@gmail.com>
2021-11-11 20:18:55 +0100

    final commit

--- main.py
+++ main.py
@@ -11,10 +11,13 @@ crypto_currency = 'BTC'
 against_currency = 'USD'
 start = dt.datetime(2016, 1, 1)
 end = dt.datetime.now()
-data = web.Datareader(
-    f'{crypto_currency}-{against_currency}', 'yahoo', start, end)
+yf.pdr_override()
+
+data = pdr.get_data_yahoo(f'{crypto_currency}-{against_currency}', start, end)
+
+
 # prepare the data
-scaler = MinMaxScaler(features_range=(0, 1))
+scaler = MinMaxScaler(feature_range=(0, 1))
 scaler_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
 prediction_days = 60
 x_train, y_train = [], []
@@ -23,12 +26,11 @@ for x in range(prediction_days, len(scaler_data)):
     y_train.append(scaler_data[x, 0])
 
 x_train, y_train = np.array(x_train), np.array(y_train)
-x_train = reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
 # building the model (LSTM MODEL)
 model = Sequential()
-model.add(LSTM(unit=50, return_sequences=True,
-          input_shape=(x_train.shape[1], 1)))
+model.add(LSTM(units=50, return_sequences=True,input_shape=(x_train.shape[1], 1)))
 model.add(Dropout(0.2))
 model.add(LSTM(units=50, return_sequences=True))
 model.add(Dropout(0.2))
@@ -36,14 +38,15 @@ model.add(LSTM(units=50))
 model.add(Dropout(0.2))
 model.add(Dense(units=1))
 
-model.compile(optimazer='adam', loss='mean_squared_error')
+model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, epochs=25, batch_size=32)
 
 # testing the model
 test_start = dt.datetime(2020, 1, 1)
 test_end = dt.datetime.now()
-test_data = web.Datareader(
-    f'{crypto_currency}-{against_currency}', 'yahoo', test_start, test_end)
+
+test_data = pdr.get_data_yahoo(f'{crypto_currency}-{against_currency}', test_start, test_end)
+
 actual_prices = test_data['Close'].values
 
 total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
@@ -59,7 +62,7 @@ for x in range(prediction_days, len(model_inputs)):
     x_test.append(model_inputs[x-prediction_days:x, 0])
 
 x_test = np.array(x_test)
-x_test = np.reshape(x_test, (x_test.shape[1]), 1)
+x_test = np.reshape(x_test,(x_test.shape[0], x_test.shape[1], 1))
 
 prediction_prices = model.predict(x_test)
 prediction_prices = scaler.inverse_transform(prediction_prices)
@@ -74,3 +77,15 @@ plt.xlabel('TIME')
 plt.ylabel('Price prediction')
 plt.legend(loc='upper left')
 plt.show()
+
+#prediction of the next day 
+
+real_data=[model_inputs[len(model_inputs)+2-prediction_days:len(model_inputs)+1,0]]
+real_data=np.array(real_data)
+real_data = np.reshape(real_data,(real_data.shape[0], real_data.shape[1], 1))
+prediction=model.predict(real_data)
+prediction=scaler.inverse_transform(prediction)
+
+
+print("the prediction of the next day")
+print(prediction)
